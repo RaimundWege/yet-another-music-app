a52e1ac233c0fe75bd2b78cba81bb894bea768ea
raimundwege <raimundwege@web.de>
2013-09-19 11:21:16 +0200

    Initial Commit

---
+++ Guitar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Guitar
+//
+//  Created by Raimund on 19.09.13.
+//  Copyright (c) 2013 Raimund Wege. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Guitar/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Guitar
+//
+//  Created by Raimund on 19.09.13.
+//  Copyright (c) 2013 Raimund Wege. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
